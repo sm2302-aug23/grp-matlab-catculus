49a73c2f99c0f8e1b673584a1c67cb839e43744b
AmeliaElam <20b2072@ubd.edu.bn>
2023-11-03 15:55:27 +0800

    plots of lia

---
+++ plots_lia.m
@@ -0,0 +1,24 @@
+% Define data points for x and y coordinates
+x=[0.50 0.75 1.40 1.50 1.25 1.00 1.125 1.50 1.75 2.40 2.50 2.25 2.125 2.50 2.75 3.125 3.50 4.00 3.25 3.125 3.60 4.00 4.125 4.20 4.0 4.0 4.50];
+y=[1.25 1.75 3.25 3.75 3.50 2.25 1.000 0.75 1.00 2.25 2.75 2.50 1.250 0.75 1.00 2.000 2.75 2.75 2.50 1.250 0.75 1.25 1.750 2.80 2.5 1.5 0.75];
+
+n=length(x);
+t= 0:n-1; % Parametric coordinate t
+tt= 0:0.01:n-1; % More dense coor tt fir spline interpolation
+
+% Compute spline interpolation
+xx= spline(t,x,tt);
+yy= spline(t,y,tt);
+
+% Plot settings
+figure(1)
+plot(xx,yy,'k','Linewidth',1.5) % plot spline
+hold on 
+plot (x,y,'bo','MarkerFaceColor','b') % plot data points
+axis([-0.2 6 -0.2 4.5])
+grid on
+title('Spline Interpolation of Cursive lia')
+xlabel('x')
+ylabel('y')
+set(gca,'FontSize',10,'LineWidth',1)
+grid on
