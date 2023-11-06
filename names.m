a17f435459e1197401b1cc0ea7d7f98388e7b3fc
syirahomar <20b2071@ubd.edu.bn>
2023-11-06 17:25:34 +0800

    added syir into names file

--- names.m
+++ names.m
@@ -22,6 +22,18 @@ tt2= 0:0.01:n2-1; % More dense coor tt fir spline interpolation
 xx2= spline(t2,x2,tt2);
 yy2= spline(t2,y2,tt2);
 
+% Define data points for x and y coordinates for Syir
+x3 = [1.5 1 0.5 1.25 1.4 1 0.5 0.75 2.25 2.25 2.4 3 3 3 3 2.5 3.3 3.8 3.6 3.65 4 4.3 4.5 4.8 4.75 4.6 5.3 5.2 5.25 5.4 5.5 5.6];
+y3 = [4.1 4.5 3.5 2.1 1.5 0.9 1.5 2.2 3 2.5 2 2.5 3 1.6 1 1.3 2.3 2.8 2.4 1.8 1.4 1.7 2.3 2.6 2.8 2.65 2.6 2.3 1.5 1.2 1.5 1.6];
+
+n3 = length(x3);
+t3 = 0:n3-1;
+tt3 = 0:0.1:n3-1;
+
+xx3 = spline(t3, x3, tt3);
+yy3 = spline(t3, y3, tt3);
+
+
 
 % Plot settings
 figure(1)
@@ -54,4 +66,13 @@ grid on
 
 % Plotting the Spline Interpolation of Cursive Syir
 subplot(2,2,3)
-
+plot(xx3, yy3, 'k', 'LineWidth', 1.5)
+hold on
+plot (x3, y3, 'bo', 'MarkerFaceColor', 'b')
+axis([-0.2 6 -0.2 4.5])
+grid on
+title('Spline interpolation of cursive Syir')
+xlabel('x')
+ylabel('y')
+set(gca, 'FontSize', 10, 'LineWidth', 1)
+grid on
