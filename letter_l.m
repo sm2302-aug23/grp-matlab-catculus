b8e1ad1fa969bafa0e6e020c48111b720bf8a6e9
suriyna <20b2067@ubd.edu.bn>
2023-11-06 22:52:56 +0800

    created letter l and edited some files

--- letter_l.m
+++ letter_l.m
@@ -4,7 +4,7 @@ y=[1.25 1.75 3.25 3.75 3.50 2.25 1.000 0.75];
 
 n=length(x);
 t= 0:n-1; % Parametric coordinate t
-tt= 0:0.01:n-1; % More dense coor tt fir spline interpolation
+tt= 0:0.01:n-1; % More dense coordinate tt for spline interpolation
 
 % Compute spline interpolation
 xx= spline(t,x,tt);
@@ -17,7 +17,7 @@ hold on
 plot (x,y,'bo','MarkerFaceColor','b') % plot data points
 axis([-0.2 6 -0.2 4.5])
 grid on
-title('Spline Interpolation of Cursive lia')
+title('Spline Interpolation of Cursive l')
 xlabel('x')
 ylabel('y')
 set(gca,'FontSize',10,'LineWidth',1)
